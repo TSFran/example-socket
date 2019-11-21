140337ba4a100b8a1dd7c99ab11e6f5485c0ca25
Luis Tupa <luis.tupa@orbis.com.pe>
2019-11-21 15:30:06 -0500

    feat(app): add example with socket.io

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = 3002;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log('Ready localhost on port: ' + port);
+}); 
